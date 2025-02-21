58f018ab7317788391be035a0fe148692be7fa53
leonardo <leonardofranciscocustodio@gmail.com>
2025-02-21 13:56:22 -0400

    API_URl utilizada pode ser local ou em deploy de produção

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/filmes";
+// Verifica se a API está rodando localmente, senão cai no deploy de produção
+const API_URL = 
+  window.location.hostname === 'localhost' 
+    ? "http://localhost:5000/api/filmes" 
+    : "https://cinelithub-production.up.railway.app/api/filmes";
 
 const api = axios.create({
   baseURL: API_URL,
